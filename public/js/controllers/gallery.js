32a54c3b829fe0abae8fd8687277895c270e77e4
harishkumarchellappa <harishkumarchellappa@gmail.com>
2015-12-06 02:24:25 +0530

    sharing fixed in gallery page

--- public/js/controllers/gallery.js
+++ public/js/controllers/gallery.js
@@ -14,7 +14,7 @@ angular.module('sbFrontEnd') .controller('GalleryCtrl', function($scope, $state,
 
 $scope.pin = function(){
 window.open(
-'//pinterest.com/pin/create/button?media=' + $scope.post.cover || $scope.vendor.gallery[0] + '&description=' + $scope.post.description || $scope.vendor.business.name + '&url='+ $scope.url, 'facebook-share-dialog',
+'//pinterest.com/pin/create/button?media=' + $scope.gallery.cover + '&description=' + $scope.gallery.description || $scope.vendor.business.name + '&url='+ $scope.url, 'facebook-share-dialog',
 'width=750,height=288');    
 return false;
 }
@@ -27,12 +27,12 @@ return false;
    $scope.shareFacebook = function(){
 FB.ui({
 method: 'feed',
-name: $scope.post.title || $scope.vendor.business.name,
+name: $scope.gallery.title || $scope.vendor.business.name,
 link: $scope.url,
-picture: $scope.post.cover || $scope.vendor.gallery[0],
+picture: $scope.gallery.cover || $scope.vendor.gallery[0],
 source: '',
-caption: $scope.post.tagline || $scope.vendor.business.name,
-description: $scope.post.description || $scope.vendor.category[0],
+caption: $scope.gallery.tagline || $scope.vendor.business.name,
+description: $scope.gallery.description || $scope.vendor.category[0],
 message: ''
 }, function(response){
   console.log(response);
