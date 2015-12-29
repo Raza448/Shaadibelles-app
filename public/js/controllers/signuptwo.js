d1c4f01d1b8dfed5f2524741528ffc369e7e35e3
Raza448 <raza@wy-digital.com>
2015-12-29 14:28:02 +0530

    Fixed #91 resolved

--- public/js/controllers/signuptwo.js
+++ public/js/controllers/signuptwo.js
@@ -130,7 +130,7 @@ var text = "";
 
   $scope.upload = function(file, insertAction) {
   
-    if(file){
+if(file){
 $rootScope.startLoading();
      File.upload(file).success(function(res) {
         var containerName = res.result.files.img[0].container;
@@ -195,6 +195,69 @@ $http.get(window.remote + '/api/vendors').then(function(res){
            $scope.postPublished = true;
           $rootScope.stopLoading();
       });
+    };
+	
+$scope.today = function() {
+      $scope.dt = new Date();
+    };
+    $scope.today();
+
+    $scope.clear = function() {
+      $scope.dt = null;
+    };
+
+    // Disable weekend selection
+    $scope.disabled = function(date, mode) {
+      return (mode === 'day' && (date.getDay() === 0 || date.getDay() === 6));
+    };
+
+    $scope.toggleMin = function() {
+      $scope.minDate = $scope.minDate ? null : new Date();
+    };
+    $scope.toggleMin();
+
+    $scope.open = function($event) {
+      $event.preventDefault();
+      $event.stopPropagation();
+
+      $scope.opened = true;
+    };
+
+    $scope.dateOptions = {
+      formatYear: 'yy',
+      startingDay: 1
+    };
+
+    $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+    $scope.format = $scope.formats[0];
+
+    var tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    var afterTomorrow = new Date();
+    afterTomorrow.setDate(tomorrow.getDate() + 2);
+    $scope.events = [{
+      date: tomorrow,
+      status: 'full'
+    }, {
+      date: afterTomorrow,
+      status: 'partially'
+    }];
+
+    $scope.getDayClass = function(date, mode) {
+      if (mode === 'day') {
+        var dayToCheck = new Date(date).setHours(0, 0, 0, 0);
+
+        for (var i = 0; i < $scope.events.length; i++) {
+          var currentDay = new Date($scope.events[i].date).setHours(0, 0, 0,
+            0);
+
+          if (dayToCheck === currentDay) {
+            return $scope.events[i].status;
+          }
+        }
+      }
+
+      return '';
     };
 
 
